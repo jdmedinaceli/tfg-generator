23129780870749f9127e48df640d15930bb2ec0e
Maluzzz <maaluuz@gmail.com>
2020-10-30 17:43:44 +0100

    add little fixes and A. contributions

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import {Tipo,Cuando,Porque,Cosa, } from './Titles'
 class App extends Component {
 
   render() {
-      const max = Object.keys(Tipo).length +1
+      const maxTipo = Object.keys(Tipo).length +1
+      const maxCuando = Object.keys(Cuando).length +1
+      const maxPorque = Object.keys(Porque).length +1
+      const maxCosa = Object.keys(Cosa).length +1
     return (
       <div class="App">
-        <div id="title"><h1>{Tipo[getRandomArbitrary(1,max)] +' '+  Cosa[getRandomArbitrary(1,max)] + Cuando[getRandomArbitrary(1,max)] + Porque[getRandomArbitrary(1,max)] }</h1>
+        <div id="title"><h1>{Tipo[getRandomArbitrary(1,maxTipo)] +' '+  Cosa[getRandomArbitrary(1,maxCosa)] + Cuando[getRandomArbitrary(1,maxCuando)] + Porque[getRandomArbitrary(1,maxPorque)] }</h1>
       </div></div>
     );
   }
--- src/Titles.js
+++ src/Titles.js
@@ -2,14 +2,20 @@ export const Tipo = {
   1: "Estudio y análisis de ",
   2: "La obra gráfica de",
   3: "Recuerdo e identidad de ",
-  4: "Análisis psicológico de "
+  4: "Análisis psicológico de ",
+  5: "Análisis y simulación de ",
+  6: "Integración de un sistema embebido para ",
+  7: "Estudio sobre la creación y evolución de "
 }
 
 export const Cosa = {
   1: "la serie The Boys ",
   2: "María la portuguesa ",
   3: "la isla de las tentaciones ",
-  4: "tu madre "
+  4: "tu madre ",
+  5: "las pelusas ",
+  6: "Picaportes ",
+  7: "Los Simpson "
   
 }
 
@@ -17,12 +23,18 @@ export const Cuando = {
   1: "durante la pandemia ",
   2: "en el siglo III ",
   3: "futuramente ",
-  4: "en el pasado "
+  4: "en el pasado ",
+  5: "en Europa Occidental ",
+  6: "en un piso ",
+  7: "en la Rusia Soviética "
 }
 
 export const Porque = {
   1: "y sus consecuencias en las telecomunicaciones ",
   2: "y las relaciones con la salud mental ",
   3: "de manera diferencial ",
-  4: "en España "
+  4: "en España ",
+  5: "y su influencia en el pensamiento contemporáneo ",
+  6: "en la guerra electrónica ",
+  7: "y paralelo al gobierno de Putín."
 }
