c67494cb60bf4b828258c5add3b9f01a98423316
MariLuz <maaluuz@gmail.com>
2020-10-30 18:10:27 +0100

    add more quotes (#4)

--- src/Titles.js
+++ src/Titles.js
@@ -5,7 +5,9 @@ export const Tipo = {
   4: "Análisis psicológico de ",
   5: "Análisis y simulación de ",
   6: "Integración de un sistema embebido para ",
-  7: "Estudio sobre la creación y evolución de "
+  7: "Estudio sobre la creación y evolución de ",
+  8: "Difractometría y porosimetría de ",
+  9: "Panorama histórico de "
 }
 
 export const Cosa = {
@@ -15,26 +17,31 @@ export const Cosa = {
   4: "tu madre ",
   5: "las pelusas ",
   6: "Picaportes ",
-  7: "Los Simpson "
+  7: "Los Simpson ",
+  8: "la bachata ",
+  9: "los memes "
   
 }
 
 export const Cuando = {
   1: "durante la pandemia ",
   2: "en el siglo III ",
-  3: "futuramente ",
+  3: "en la próxima década ",
   4: "en el pasado ",
   5: "en Europa Occidental ",
   6: "en un piso ",
-  7: "en la Rusia Soviética "
+  7: "en la Rusia Soviética ",
+  8: "mientras la represión ",
+  9: "en España "
 }
 
 export const Porque = {
   1: "y sus consecuencias en las telecomunicaciones ",
   2: "y las relaciones con la salud mental ",
   3: "de manera diferencial ",
-  4: "en España ",
+  4: ": Factores determinantes.",
   5: "y su influencia en el pensamiento contemporáneo ",
   6: "en la guerra electrónica ",
-  7: "y paralelo al gobierno de Putín."
+  7: "y paralelo al gobierno de Putín.",
+  8: "y la hipersexualización televisiva."
 }
