6573ea2293534e25de494752a0dea7b17b18c90c
jtrancoso <jesus.trancoso.jimenez@gmai.com>
2020-10-31 10:22:17 +0100

    first addition

--- Titles.js
+++ Titles.js
@@ -1,19 +1,23 @@
 export const Tipo = {
   1: "Estudio y análisis de",
   2: "La obra gráfica de"
+  3: "Evolución de"
 }
 
 export const Cosa = {
   1: "la serie the boys",
   2: "María la portuguesa"
+  3: "el fútbol europeo"
 }
 
 export const Cuando = {
   1: "durante la pandemia",
   2: "en el siglo III"
+  3: "en la era de la información"
 }
 
 export const Porque = {
   1: "y sus consecuencias en las telecomunicaciones",
   2: "y las relaciones con la salud mental"
+  3: "y su efecto en la sociedad"
 }
